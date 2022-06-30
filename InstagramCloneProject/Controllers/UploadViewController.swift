a32d49bb77a750af432b32d70b941abdcb687c44
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-30 21:33:10 +0300

    UpdateViewController managed, photo selecting feature added

--- InstagramCloneProject/Controllers/UploadViewController.swift
+++ InstagramCloneProject/Controllers/UploadViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class UploadViewController: UIViewController {
 
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var commentTextfield: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(tapGesture)
 
-        // Do any additional setup after loading the view.
+
+    }
+    
+    @objc func chooseImage() {
+        
+        //Selecting image, accessing to user library
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        
     }
     
 
+}
+
+
+extension UploadViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
