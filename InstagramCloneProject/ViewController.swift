7847783e3cdd0a399fa9c366a6dbf2c8030ffe92
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-30 20:24:13 +0300

    UI Set, Welcome View Set, basic segues created

--- InstagramCloneProject/ViewController.swift
+++ InstagramCloneProject/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var emailTextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInButtonClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+    }
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
 }
 
