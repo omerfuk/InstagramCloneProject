0cef8353a2e412eb2249556b9a64f3ae905c642a
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-02 00:53:18 +0300

    getting datas from firebase and showing them on tableView

--- InstagramCloneProject/Controllers/FeedViewController.swift
+++ InstagramCloneProject/Controllers/FeedViewController.swift
@@ -7,16 +7,69 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 class FeedViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         
+        getDataFromFirestore()
+        
+    }
+    
+    func getDataFromFirestore() {
+        
+        
+        let fireStoreDatabase = Firestore.firestore()
+        /*
+        let settings = fireStoreDatabase.settings
+        fireStoreDatabase.settings = settings
+        */
+        
+        fireStoreDatabase.collection("Posts").addSnapshotListener { snapshot, error in
+            
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else{
+                //No error
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    for document in snapshot!.documents {
+                        let documentID = document.documentID
+                        print(documentID)
+                        
+                        if let postedBy = document.get("postedBy") as? String{
+                            self.userEmailArray.append(postedBy)
+                        }
+                        
+                        if let postComment = document.get("postComment") as? String{
+                            self.userCommentArray.append(postComment)
+                        }
+                        
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        
+                        if let imageUrl = document.get("imageUrl") as? String{
+                            self.userImageArray.append(imageUrl)
+                        }
+                    }
+                    //right after for loop finished
+                    self.tableView.reloadData()
+                }
+                
+            }
+        }
     }
 
 
@@ -25,14 +78,14 @@ class FeedViewController: UIViewController {
 extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell
-        cell.userEmailLabel.text = "test@gmail.com"
-        cell.likeLabel.text = "0"
-        cell.commentLabel.text = "comment"
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
         cell.userImageView.image = UIImage(named: "clickMe")
         
         return cell
